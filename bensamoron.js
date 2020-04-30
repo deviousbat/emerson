d5d769816cc051e0fbb86deaf358f2a890feaded
deviousbat <64614939+deviousbat@users.noreply.github.com>
2020-04-30 18:24:40 -0400

    Add files via upload

--- bensamoron.js
+++ bensamoron.js
@@ -1,9 +1,6 @@
-exports.makethiswrong = function (value) {
+export.makethiswrong = function test(value) {
     if (typeof value === "boolean") {
-        const varname = Object.keys({
-            value
-        })[0]
-        console.log(varname + " is " + !value)
+        console.log("This is " + !value)
     } else {
         throw "This is too complex a value for me to be wrong about."
     }
